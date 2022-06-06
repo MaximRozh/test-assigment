4e7e4ebf80b706604e7eee2acfc52a5dc25d1314
Maksym_Rozhanskyi <rozhanskiy333@ukr,net>
2022-06-06 23:25:55 +0300

    test

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = withPWA({
     dest: "public",
     register: true,
     skipWaiting: true,
-    runtimeCaching,
+    // runtimeCaching,
     disable: process.env.NODE_ENV === "development",
   },
 });
