d8993b1e19ae8c8fd76b7d0e3f347a6f30b4be78
Maksym_Rozhanskyi <rozhanskiy333@ukr,net>
2022-06-09 02:34:15 +0300

    remove

--- src/pages/fallback.tsx
+++ src/pages/fallback.tsx
@@ -3,11 +3,15 @@ import style from "../styles/Fallback.module.scss";
 const fallback = () => {
   return (
     <div className={style["fallback-container"]}>
-      <h1 className={style["text"]}>
-        Sorry, web-page is unavailable. Check your internet connection and try
-        again
-      </h1>
-      <div className={style["fallback"]}></div>
+      <div className={style["fallback-wrapper"]}>
+        <h1 className={style["text"]}>
+          Sorry, web-page is unavailable. Check your internet connection and try
+          again
+        </h1>
+        <div className={style["fallback-wrapper"]}>
+          <div className={style["fallback"]}></div>
+        </div>
+      </div>
     </div>
   );
 };
