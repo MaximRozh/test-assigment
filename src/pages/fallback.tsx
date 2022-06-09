39272ab37ca82897ffed9570de4dce3bfccd0c2b
Maksym_Rozhanskyi <rozhanskiy333@ukr,net>
2022-06-09 10:08:38 +0300

    fallback page

--- src/pages/fallback.tsx
+++ src/pages/fallback.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import style from "../styles/Fallback.module.scss";
+import { MdSignalWifiConnectedNoInternet0 } from "react-icons/md";
+
 const fallback = () => {
   return (
     <div className={style["fallback-container"]}>
@@ -7,9 +9,18 @@ const fallback = () => {
         <h1 className={style["text"]}>
           Sorry, web-page is unavailable. Check your internet connection and try
           again
+          <MdSignalWifiConnectedNoInternet0 />
         </h1>
-        <div className={style["fallback-wrapper"]}>
-          <div className={style["fallback"]}></div>
+        <div className={style["pacman-container"]}>
+          <div className={style["pacman"]}></div>
+          <div className={style["path"]}></div>
+          <div className={style["path"]}></div>
+          <div className={style["path"]}></div>
+          <div className={style["path"]}></div>
+          <div className={style["path"]}></div>
+          <div className={style["path"]}></div>
+          <div className={style["path"]}></div>
+          <div className={style["path"]}></div>
         </div>
       </div>
     </div>
